9b11f1c241993d124d98bb15e861209e40bf8ea6
Fer Cruz <cruzfernandomartin94@gmail.com>
2024-12-31 17:15:58 -0300

    Exercise 2 finished.

--- basicExercise/basicExercise/Program.cs
+++ basicExercise/basicExercise/Program.cs
@@ -10,11 +10,48 @@ namespace basicExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name: ");
+            //Exercise1();
+            Exercise2();
+        }
+
+        static void Exercise1()
+        {
+            Console.Write("Enter your name: ");
             string lineaFerchu = Console.ReadLine();
             Console.WriteLine($"{lineaFerchu}! Welcome to .NET programming!");
             Console.ReadLine();
+        }
 
+        static void Exercise2()
+        {
+            Console.Write("Enter the first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the operation (+, -, * , /): ");
+            string operation = Console.ReadLine();
+            void mathOperation(int number) {
+                Console.WriteLine($"The result is: {number}");
+            }
+            switch(operation)
+            {
+                case "+":
+                    mathOperation(firstNumber + secondNumber);
+                    break;
+                case "-":
+                    mathOperation(firstNumber - secondNumber);
+                    break;
+                case "*":
+                    mathOperation(firstNumber * secondNumber);
+                    break;
+                case "/":
+                    mathOperation(firstNumber / secondNumber);
+                    break;
+                default:
+                    Console.WriteLine("Incorrect operation, terminating program.");
+                    return;
+            }
+            Console.ReadLine();
         }
     }
 }
