938258f1d05bab80c06887d8a446d0e843f4566e
Fer Cruz <cruzfernandomartin94@gmail.com>
2024-12-31 16:25:10 -0300

    Exercise 1 - I/O console exercis. Done.

---
+++ basicExercise/basicExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string lineaFerchu = Console.ReadLine();
+            Console.WriteLine($"{lineaFerchu}! Welcome to .NET programming!");
+            Console.ReadLine();
+
+        }
+    }
+}
